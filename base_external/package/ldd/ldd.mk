2c398824ad8dd749e6ff9904f2e3934f593b658c
Biplav Poudel <biplavpoudel764@gmail.com>
2026-05-06 18:59:42 +0545

    updated the define macro to use ldd3, not ldd

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -12,13 +12,13 @@ LDD3_SITE_METHOD = git
 LDD3_GIT_SUBMODULES = YES
 LDD3_MODULE_SUBDIRS = scull misc-modules
 
-define LDD_BUILD_CMDS
+define LDD3_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/misc-modules all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/scull all
 endef
 
 # Adding our misc-modules and scull utilities/scripts and its init script to the installation steps below
-define LDD_INSTALL_TARGET_CMDS
+define LDD3_INSTALL_TARGET_CMDS
 	@echo "Installing scull driver, faulty driver and hello module from ${LDD_SITE} into rootfs directory..."
 	@(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
 	@(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
