032b381493b6df15424f3deb221edd4bf0c98ecb
Biplav Poudel <biplavpoudel764@gmail.com>
2026-05-06 22:56:39 +0545

    fixed wrong git url for assignment 7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,7 +7,7 @@
 
 #Referencing assignment7 git contents
 LDD_VERSION = 78e81c283ce49abee1117fae89e7462f4ffa0172
-LDD_SITE = git@github.com:cu-ecen-aeld/assignments-7-biplavpoudel.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-biplavpoudel.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = scull misc-modules
