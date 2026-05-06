ec59ba03f54a7882f0b46c07b94be08915626b34
Biplav Poudel <biplavpoudel764@gmail.com>
2026-05-06 20:18:42 +0545

    added ldd3 in defconfig

--- base_external/package/ldd3/ldd3.mk
+++ base_external/package/ldd3/ldd3.mk
@@ -12,16 +12,16 @@ LDD3_SITE_METHOD = git
 LDD3_GIT_SUBMODULES = YES
 LDD3_MODULE_SUBDIRS = scull misc-modules
 
-define LDD3_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/misc-modules all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/scull all
-endef
+# define LDD3_BUILD_CMDS
+# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/misc-modules all
+# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/scull all
+# endef
 
 # Adding our misc-modules and scull utilities/scripts and its init script to the installation steps below
 define LDD3_INSTALL_TARGET_CMDS
-	@echo "Installing scull driver, faulty driver and hello module from ${LDD_SITE} into rootfs directory..."
+	@echo "Installing scull driver, faulty driver and hello module from ${LDD3_SITE} into rootfs directory..."
 	@(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	@(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	@(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
 	@(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
 	@(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 endef
