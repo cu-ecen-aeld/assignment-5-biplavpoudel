d47828f703acdbb2f4fe4508fe6f1d0e11f8a8eb
Biplav Poudel <biplavpoudel764@gmail.com>
2026-05-07 12:44:55 +0545

    commented out submodule part; no need

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,7 +9,7 @@
 LDD_VERSION = a336fded1af28acee682e91b940f671be70386ed
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-biplavpoudel.git
 LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
+# LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = scull misc-modules
 
 # define LDD_BUILD_CMDS
