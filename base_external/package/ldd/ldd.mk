884c22447659f0375ee9190b524211c2cc239002
Biplav Poudel <biplavpoudel764@gmail.com>
2026-05-07 14:52:47 +0545

    made s98 init module executable, and deleted the unneeded install part in mk file for ldd

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,22 +9,8 @@
 LDD_VERSION = a336fded1af28acee682e91b940f671be70386ed
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-biplavpoudel.git
 LDD_SITE_METHOD = git
-# LDD_GIT_SUBMODULES = YES
-LDD_MODULE_SUBDIRS = scull misc-modules
-
-# define LDD_BUILD_CMDS
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/misc-modules all
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/scull all
-# endef
-
-# Adding our misc-modules and scull utilities/scripts and its init script to the installation steps below
-define LDD_INSTALL_TARGET_CMDS
-	@echo "Installing scull driver, faulty driver and hello module from ${LDD_SITE} into rootfs..."
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-endef
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = misc-modules scull 
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
