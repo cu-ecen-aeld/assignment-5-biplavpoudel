3c9132d4689fe5ee17f83820894f2246b4d0018c
Biplav Poudel <biplavpoudel764@gmail.com>
2026-05-07 16:25:26 +0545

    fix:forgot to install modules in /usr/bin first

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -12,5 +12,19 @@ LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules scull 
 
+# define LDD_BUILD_CMDS
+# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/misc-modules all
+# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/scull all
+# endef
+
+# Adding our misc-modules and scull utilities/scripts and its init script to the installation steps below
+define LDD_INSTALL_TARGET_CMDS
+	@echo "Installing scull driver, faulty driver and hello module from ${LDD_SITE} into rootfs..."
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
